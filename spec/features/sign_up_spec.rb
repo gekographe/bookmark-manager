62a7b0ce0343d8c33f3126852c16caed2baca89d
Simba14 <seanblundell@gmail.com>
2017-04-07 15:44:16 +0100

    Prevented duplication of emails

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -9,14 +9,24 @@ feature "User Sign Up" do
   scenario "user signs up with a mismatching password" do
     expect { sign_up_fail(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(page).to have_current_path('/users')
-    expect(page).to have_content('Password and confirmation password do not match')
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   scenario "user signs up without email address" do
     expect { sign_up_no_email }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
-  scenario "I can't sign up with an invalid email address" do
+  scenario "user can't sign up with an invalid email address" do
     expect { sign_up_fail(email: 'invalid@email')}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario "user can't sign up with an existing email address" do
+    sign_up_fail
+    expect { sign_up_fail }.not_to change(User, :count)
+    expect(page).to have_content("Email is already taken")
   end
 end
